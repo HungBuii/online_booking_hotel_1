39aa80539235e9f32ef6fdde452c9d02fc7a7868
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-03-20 16:29:42 +0700

    update 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Front\AboutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfileController;
@@ -16,9 +18,9 @@ use App\Http\Controllers\Admin\AdminProfileController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Front */
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
 
 /* Admin */
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
